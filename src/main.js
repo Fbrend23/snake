29a4bfc631514c8dc0a86380e1235b617e39dd11
Fbrend23 <fbrend23@gmail.com>
2025-02-28 11:15:52 +0100

    Feat main
    Lancement du jeu avec espace et ajout d'un texte sur la page html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { initSnake, moveSnake, drawSnake } from "./snake.js";
-// import { generateFood, drawFood } from "./food.js";
+import { generateFood, drawFood } from "./food.js";
 import { handleDirectionChange } from "./controls.js";
 // import { checkCollision, checkWallCollision } from "./collision.js";
 // import { drawScore } from "./score.js";
@@ -15,23 +15,30 @@ let direction = "RIGHT"; // Direction initial du serpent
 let score = 0;
 let gameInterval; // Variable pour stocker l'identifiant de l'intervalle
 
+
+//Lancer le jeu avec Espace ou gère les directions du serpent
 document.addEventListener("keydown", (event) => {
-  direction = handleDirectionChange(event, direction);
+  if (event.code === "Space" && !gameInterval) {
+    startGame();
+  } else {
+    direction = handleDirectionChange(event, direction);
+  }
 });
-//************** Main **************//
-startGame();
 
 function startGame() {
-  console.log(snake)
-  snake = initSnake(box);
-  // food = generateFood(box, canvas);
+  document.getElementById("StartGamu").textContent =""; //Efface le message d'instruction de départ
+  console.log(snake) //log pour les erreurs
+  snake = initSnake(box); //initilisation du snake
+  food = generateFood(box, canvas);
   draw();
   gameInterval = setInterval(draw, gameSpeed); // Stockage de l'identifiant de l'intervalle
+  
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Efface le canvas
   moveSnake(snake, direction, box);
   drawSnake(ctx, snake, box);
+  drawFood(ctx, food, box)
 }
 
